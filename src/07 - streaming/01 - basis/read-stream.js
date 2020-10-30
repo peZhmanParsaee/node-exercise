25e5d95c80aa8aacbe9e273fc48560ff5d7a4720
pezhmanparsaee <p.parsaee@gmail.com>
2020-10-30 18:54:28 +0330

    organize sub files of 07 - streaming

--- src/07 - streaming/01 - basis/read-stream.js
+++ src/07 - streaming/01 - basis/read-stream.js
@@ -1,9 +1,11 @@
-var fs = require("fs");
+const fs = require("fs");
 
 var stream = fs.createReadStream("./resource.json");
+
 stream.on("data", function (chunk) {
   console.log(chunk);
 });
+
 stream.on("end", function () {
   console.log("finished");
 });
