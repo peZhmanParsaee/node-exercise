cde0927d699b3552ef76da03ac5540ca2d349cf0
pezhmanparsaee <p.parsaee@gmail.com>
2021-03-16 14:05:05 +0330

    add more samples to 13-clustering

---
+++ src/13-clustering/index3.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const Worker = require('webworker-threads').Worker;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  const worker = new Worker(function() {
+    this.onmessage = function() {
+      let counter = 0;
+      while (counter < 1e9) { counter++ }
+
+      this.postMessage(counter);
+    };
+  });
+
+  worker.onmessage = function(message) {
+    console.log(message.data);
+    res.send('' + message.data);
+  };
+
+  worker.postMessage();
+});
+
+app.get('/fast', (req, res) => {
+  res.send('fast');
+});
+
+app.listen(3000);
